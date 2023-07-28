23accb4e33b7cc07c32675b344dbec6bc5b64565
maryam-sulayman <muhammedmariam80@yahoo.co.uk>
2023-07-28 22:26:48 +0100

    integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Hello ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordValue(event) {
